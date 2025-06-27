fdce38ba471e0d03b49b59f0b5bf215e428becc8
Whysoezzy <n.mamonovv@gmail.com>
2025-06-27 16:57:50 +0300

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Meet"
+include(":app")
+include(":core:ui")
+include(":core:data")
+include(":core:domain")
+include(":uikit")
+include(":features:features-main")
