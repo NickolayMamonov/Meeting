f57d74d714d5b7f63109016d38c1c29cb33c44b3
whysoezzy <n.mamonovv@gmail.com>
2025-12-10 02:35:19 +0300

    add dateformatter and validation of phonenumber,smscode,name

---
+++ core/common/src/main/java/com/whysoezzy/common/utils/DateUtils.kt
@@ -0,0 +1,25 @@
+package com.whysoezzy.common.utils
+
+import java.time.Instant.ofEpochMilli
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+object DateUtils {
+    private val dateFormatter = DateTimeFormatter.ofPattern("dd MMMM")
+
+    fun formatDate(dateTime: LocalDateTime): String {
+        return dateTime.format(dateFormatter)
+    }
+
+    fun toTimestamp(dateTime: LocalDateTime): Long {
+        return dateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()
+    }
+
+    fun fromTimestamp(timestamp: Long): LocalDateTime {
+        return LocalDateTime.ofInstant(
+            ofEpochMilli(timestamp),
+            ZoneId.systemDefault()
+        )
+    }
+}
