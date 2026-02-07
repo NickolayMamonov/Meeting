f4c4d0f1ec81b947b3afaab58e46c1921fea0251
whysoezzy <whysoezzy@gmail.com>
2026-02-07 18:07:31 +0300

    fix code analysis

--- features/meetings/presentation/src/main/java/dev/whysoezzy/meetings/participants/presentation/MeetingParticipantsViewModel.kt
+++ features/meetings/presentation/src/main/java/dev/whysoezzy/meetings/participants/presentation/MeetingParticipantsViewModel.kt
@@ -2,7 +2,6 @@ package dev.whysoezzy.meetings.participants.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.whysoezzy.domain.models.Person
 import com.whysoezzy.domain.usecase.GetMeetingByIdUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
