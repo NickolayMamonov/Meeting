340f66bae3fbcdb71a82476d37681b490b908e7a
whysoezzy <whysoezzy@gmail.com>
2026-02-10 01:46:09 +0300

    transfer dto models to core:data

--- core/data/src/main/java/com/whysoezzy/data/dto/MeetingDto.kt
+++ core/data/src/main/java/com/whysoezzy/data/dto/MeetingDto.kt
@@ -6,17 +6,17 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class MeetingDto(
     @SerialName("id") val id: Long,
-    @SerialName("imageUrl") val imageUrl: String,  // camelCase
+    @SerialName("imageUrl") val imageUrl: String,
     @SerialName("title") val title: String,
     @SerialName("description") val description: String,
     @SerialName("time") val time: Long,
     @SerialName("date") val date: String,
     @SerialName("address") val address: MeetingAddressDto,
     @SerialName("tags") val tags: List<MeetingTagDto>,
-    @SerialName("personHost") val personHost: PersonHostDto?,  // camelCase + nullable
-    @SerialName("communityHost") val communityHost: CommunityHostDto?,  // camelCase + nullable
+    @SerialName("personHost") val personHost: PersonHostDto?,
+    @SerialName("communityHost") val communityHost: CommunityHostDto?,
     @SerialName("participants") val participants: List<PersonDto>,
-    @SerialName("meetingStatus") val meetingStatus: String,  // camelCase
-    @SerialName("isUserInParticipants") val isUserInParticipants: Boolean,  // camelCase
+    @SerialName("meetingStatus") val meetingStatus: String,
+    @SerialName("isUserInParticipants") val isUserInParticipants: Boolean,
     @SerialName("capacity") val capacity: Int?
 )
