615b4eb98542956e37ea97b7938a83b012de28d0
whysoezzy <whysoezzy@gmail.com>
2026-04-26 17:30:27 +0300

    fix compile sdk version and unused imports

--- features/profile/presentation/src/main/java/dev/whysoezzy/profile/details/presentation/ProfileDetailsScreen.kt
+++ features/profile/presentation/src/main/java/dev/whysoezzy/profile/details/presentation/ProfileDetailsScreen.kt
@@ -2,7 +2,6 @@ package dev.whysoezzy.profile.details.presentation
 
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
