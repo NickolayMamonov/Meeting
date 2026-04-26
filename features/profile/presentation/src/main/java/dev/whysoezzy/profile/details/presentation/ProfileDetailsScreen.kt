df9d6ac8e78e9498d185a17fb9497ff19b274eb0
whysoezzy <whysoezzy@gmail.com>
2026-04-26 18:42:33 +0300

    add Profile screens instead of stubs

--- features/profile/presentation/src/main/java/dev/whysoezzy/profile/details/presentation/ProfileDetailsScreen.kt
+++ features/profile/presentation/src/main/java/dev/whysoezzy/profile/details/presentation/ProfileDetailsScreen.kt
@@ -2,6 +2,7 @@ package dev.whysoezzy.profile.details.presentation
 
 import android.content.Context
 import android.content.Intent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
