477d093404ae223d2e56e78cf73e2ce1f07a0d18
whysoezzy <whysoezzy@gmail.com>
2026-05-13 02:55:39 +0300

    fix: delete old imports

--- core/network/src/main/java/com/whysoezzy/network/KtorNetworkModule.kt
+++ core/network/src/main/java/com/whysoezzy/network/KtorNetworkModule.kt
@@ -1,6 +1,5 @@
 package com.whysoezzy.network
 
-import android.util.Log
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.HttpTimeout
@@ -10,7 +9,6 @@ import io.ktor.client.plugins.auth.providers.bearer
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
