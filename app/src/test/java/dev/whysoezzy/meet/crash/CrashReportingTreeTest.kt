ad3ba9136c9f1fcd953ece80c552cd0783b91185
whysoezzy <whysoezzy@gmail.com>
2026-06-01 01:22:29 +0300

    test(app): cover CrashReportingTree breadcrumb/exception forwarding

---
+++ app/src/test/java/dev/whysoezzy/meet/crash/CrashReportingTreeTest.kt
@@ -0,0 +1,47 @@
+package dev.whysoezzy.meet.crash
+
+import com.whysoezzy.common.crash.CrashReporter
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import timber.log.Timber
+
+class CrashReportingTreeTest {
+    private val reporter = mockk<CrashReporter>(relaxed = true)
+    private lateinit var tree: CrashReportingTree
+
+    @Before
+    fun setUp() {
+        tree = CrashReportingTree(reporter)
+        Timber.plant(tree)
+    }
+
+    @After
+    fun tearDown() {
+        Timber.uproot(tree)
+    }
+
+    @Test
+    fun `WARN forwarded as breadcrumb, no exception`() {
+        Timber.tag("Tag").w("warn msg")
+        verify { reporter.log("[Tag] warn msg") }
+        verify(exactly = 0) { reporter.recordException(any()) }
+    }
+
+    @Test
+    fun `ERROR with throwable records exception`() {
+        val ex = RuntimeException("boom")
+        Timber.tag("Tag").e(ex, "err msg")
+        verify { reporter.log("[Tag] err msg") }
+        verify { reporter.recordException(ex) }
+    }
+
+    @Test
+    fun `INFO below WARN threshold - nothing forwarded`() {
+        Timber.tag("Tag").i("info msg")
+        verify(exactly = 0) { reporter.log(any()) }
+        verify(exactly = 0) { reporter.recordException(any()) }
+    }
+}
